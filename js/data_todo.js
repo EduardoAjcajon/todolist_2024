b96ba5b929db2644bc67bcc0e901c2ab09a55864
EduardoAjcajon <eaajcajon@scl.edu.gt>
2024-03-15 10:41:20 -0600

    ventana modal funcional

--- js/data_todo.js
+++ js/data_todo.js
@@ -2,23 +2,8 @@ let tareas = [
     {
         estado : true,
         id: 1,
-        texto: "Quitar todos los tornillos de la tap",
-    },
-    {
-        estado: false,
-        id: 2,
-        texto: "levantar la tapa con cuidado",
-    },
-    {
-        estado: true,
-        id: 3,
-        texto: "Desconectar la batería",
-    },
-    {
-        estado: false,
-        id: 4,
-        texto: "Identificar componentes",
+        texto: "Quitar todos los tornillos de la tapa",
     }
 ]
 
-export {tareas};
+export {tareas};    
--- js/main.js
+++ js/main.js
@@ -32,18 +32,63 @@ cargar_tareas();
 function cargar_botones () {
     let caja_btn = document.querySelector('.botones');
 
-    caja_btn.innerHTML = `<div class="btn"> + </div>`
+    caja_btn.innerHTML = `
+        <div class="contenerdor_btn">
+            <div class="btn"> + </div>
+            <p class="txt_nuevo"> Nuevo </p> 
+        </div>
+    `
 }
 
 cargar_botones();
 
 function cargar_formulario () {
     let ventana_formulario = document.querySelector('.formulario');
-    ventana_formulario.classList.add(`acrivar_b`)
-    ventana_formulario.innerHTML = `
+    ventana_formulario.classList.add(`acrivar_b`);
+    ventana_formulario.innerHTML = `    
+    <div class="div_formulario">
+    <div class="btn_cerar2"> x </div>
+
+    <input type="txt" class="entrada_tarea">
+    </div>
+
+    <div class="btn_crear">Crear</div>
 
     `;
+
+    let btn_crear = document.querySelector(".btn_crear");
+    btn_crear.addEventListener("click", ()=> {
+        let tarea = document.querySelector(".entrada_tarea").value;
+
+        let estructura_de_tarea = {
+            estado: true,
+            id: tarea,
+            texto: tarea,
+        }
+
+        tareas.push(estructura_de_tarea);
+
+        let cuadro_de_tareas = document.querySelector(".lista_tareas");
+        cuadro_de_tareas.innerHTML=`
+        <div class="transparente"></div>
+        `;
+        
+        
+        cargar_tareas( ventana_formulario.classList.remove(`acrivar_b`));
+
+        let btna_formulario2 = document.querySelector('.transparente');
+        btna_formulario2.addEventListener('click', cargar_formulario);
+
+        
+    })
+    
+    let btn_cerar2 = document.querySelector('.btn_cerar2');
+    btna_formulario.addEventListener('click', cargar_formulario);
 }
+
+let btna_formulario2 = document.querySelector('.transparente');
+btna_formulario2.addEventListener('click', cargar_formulario);
+
 
 // Programacion del boxDecorationBreak: 
 let btna_formulario = document.querySelector('.btn');
